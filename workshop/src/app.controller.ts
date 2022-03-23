eba5d49980a2ccd94758431e6bc1738bd88ddbe5
Sidi BA <sidi1.ba@epitech.eu>
2022-03-23 20:25:59 +0100

    [Problem] problem pour npm

--- workshop/src/app.controller.ts
+++ workshop/src/app.controller.ts
@@ -2,25 +2,20 @@ import {Body, Controller, Get, Post, Put, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ArticleDto } from './article.dto';
 
-@Controller()
+@Controller('article')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/:title')
+  getArticle(@Param('title') title: string): Promise<ArticleDto> {
+    return this.appService.getArticle(title);
   }
-  @Get('article/:title')
-  getArticle(@Param('title') title: string): string {
-    console.log(title);
-    return this.appService.getArticle();
+  @Post()
+  async postArticle(@Body() article : ArticleDto): Promise<void> {
+    await this.appService.createArticle(article);
   }
-  @Post('article')
-  postArticle(@Body() article : ArticleDto): void {
-    console.log(article);
-  }
-  @Put('article/:title')
-  putArticle(@Param('title') title: string, @Body() article: ArticleDto): void {
-    console.log(title, article);
+  @Put('/:title')
+  async putArticle(@Param('title') title: string, @Body() article: ArticleDto): Promise<void> {
+    await this.appService.putArticle(title, article);
   }
 }
--- workshop/src/app.service.ts
+++ workshop/src/app.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ArticleDto } from './article.dto';
+import { DatabaseService } from './database.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly databaseService: DatabaseService) {}
+  async getArticle(title: string): Promise<ArticleDto> {
+    const article = await this.databaseService.findArticle(title);
+
+    if (!article) {
+      throw new NotFoundException('Article ${title} not found.');
+    }
+    return article;
   }
-  getArticle(): string {
-    return "Article";
+
+  putArticle(title: string, updateArticle: ArticleDto): Promise<ArticleDto> {
+    return this.databaseService.updateArticle(title, updateArticle);
+  }
+
+  createArticle(newArticle: ArticleDto): Promise<ArticleDto> {
+    return this.databaseService.createArticle(newArticle);
   }
 }
